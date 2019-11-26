67eff4ce04c343f00e5b895d52f44f9e810c9799
Tomoya Koike <makeffort134@gmail.com>
2019-11-26 21:45:12 +0900

    [update] optimize.py updated for retrain

--- tasks/optimize.py
+++ tasks/optimize.py
@@ -1,18 +1,15 @@
-import pandas as pd
 import argparse
-from pathlib import Path
 from datetime import datetime
-from ml.src.metrics import Metric
+
+import optuna
+import torch
+from eeglibrary import eeg
 from eeglibrary.src.eeg_dataloader import set_dataloader as eeg_dataloader
-from ml.src.dataloader import set_dataloader, set_ml_dataloader
 from eeglibrary.src.eeg_dataset import EEGDataSet
 from eeglibrary.src.preprocessor import preprocess_args
-from eeglibrary import eeg
+from ml.src.dataloader import set_dataloader, set_ml_dataloader
+from ml.src.metrics import Metric
 from train_manager import TrainManager, train_manager_args
-import torch
-import json
-import optuna
-
 
 LABELS = {'none': 0, 'seiz': 1}
 PATIENTS = ['YJ0112PQ', 'MJ00803P', 'YJ0100DP', 'YJ0100E9', 'MJ00802S', 'YJ01133T', 'YJ0112AU', 'WJ01003H', 'WJ010024']
@@ -42,6 +39,21 @@ def load_func(path):
     return torch.from_numpy(eeg.load_pkl(path).values.reshape(-1, ))
 
 
+def select_params(trial):
+
+    # TRAIN_CONF['iterations'] = trial.suggest_int('iterations', 10, 100)
+    # TRAIN_CONF['k_disc'] = trial.suggest_int('k_disc', 5, 30)
+    # TRAIN_CONF['k_clf'] = trial.suggest_int('k_clf', 5, 30)
+
+    TRAIN_CONF['retrain'] = trial.suggest_int('retrain', 5, 30)
+    TRAIN_CONF['retrain_epochs'] = trial.suggest_int('retrain_epochs', 0, 40)
+
+    # negative_rate = trial.suggest_uniform('sample_balance', 0, 1.0)
+    # TRAIN_CONF['sample_balance'] = [negative_rate, 1.0 - negative_rate]
+    # TRAIN_CONF['batch_size'] = trial.suggest_int('batch_size', 16, 128)
+    # TRAIN_CONF['lr'] = trial.suggest_loguniform('lr', 1e-7, 1e-3)
+
+
 def objective(trial):
 
     dataset_cls = EEGDataSet
@@ -57,9 +69,7 @@ def objective(trial):
 
     TRAIN_CONF['class_names'] = list(set(LABELS.values()))
 
-    TRAIN_CONF['iterations'] = trial.suggest_int('iterations', 10, 100)
-    TRAIN_CONF['k_disc'] = trial.suggest_int('k_disc', 5, 30)
-    TRAIN_CONF['k_clf'] = trial.suggest_int('k_clf', 5, 30)
+    select_params(trial)
 
     train_manager = TrainManager(TRAIN_CONF, load_func, label_func, dataset_cls, set_dataloader_func, metrics, expt_note)
     model, val_metrics, test_metrics = train_manager.train_test()
@@ -74,7 +84,8 @@ def tuning() -> float:
     study = optuna.create_study()
     study.optimize(objective, n_trials=100)
     print(study.best_params)
-    study.trials_dataframe().to_csv('trials.csv')
+    now_time = datetime.today().strftime('%y%m%H%M')
+    study.trials_dataframe().to_csv(f'trials_{now_time}.csv')
 
 
 if __name__ == '__main__':
