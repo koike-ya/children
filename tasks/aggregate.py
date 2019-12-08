b312348fc7e68de3b32a53442a981ee7d3d963ed
Tomoya Koike <makeffort134@gmail.com>
2019-12-08 17:55:12 +0900

    [update] misc update

--- tasks/aggregate.py
+++ tasks/aggregate.py
@@ -1,20 +1,7 @@
 import argparse
-import io
-import json
-import sys
-from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
-import torch
-from eeglibrary import eeg
-from eeglibrary.src.eeg_dataloader import set_dataloader as eeg_dataloader
-from eeglibrary.src.eeg_dataset import EEGDataSet
-from eeglibrary.src.metrics import Metric
-from eeglibrary.src.preprocessor import preprocess_args
-from ml.src.dataloader import set_dataloader, set_ml_dataloader
-from ml.src.metrics import metrics2df
-from train_manager import TrainManager, train_manager_args
 
 
 def aggregate_args(parser):
@@ -26,32 +13,18 @@ def aggregate_args(parser):
 
 
 def aggregate(cfg):
+    total = len(cfg['expt_ids'].split(','))
+
     for phase in ['val', 'test']:
-        df = pd.DataFrame()
+        for i, expt_id in enumerate(cfg['expt_ids'].split(',')):
+            if i == 0:
+                df = pd.read_csv(Path(__file__).parent.parent / 'output' / 'metrics' / f"{expt_id}_{phase}.csv",
+                                 index_col=0) / total
+                continue
 
-        for expt_id in cfg['expt_ids'].split(','):
-            locals()[f'{phase}_metrics'] = pd.read_csv(Path(__file__).parent.parent / 'output' / 'metrics' / f"{expt_id}_{phase}.csv")
-
-            series = pd.Series()
-            for i, row in locals()[f'{phase}_metrics'].iterrows():
-                for col in ['mean', 'std']:
-                    series[f"{row['metric_name']}_{col}"] = row[col] * i
-            df = pd.concat([df, pd.DataFrame(series).T])
-
-        df = pd.DataFrame(df.mean().values.reshape((-1, 2)))
-        df.index = locals()[f'{phase}_metrics']['metric_name']
-        df.columns = [f'{phase} mean mean', f'{phase} std mean']
-
-        if phase == 'val':
-            val_agg_df = df
-        else:
-            test_agg_df = df
-
-    pd.concat([val_agg_df, test_agg_df], axis=1).to_csv(
-        Path(__file__).parent.parent / 'output' / 'metrics' / f"agg-{cfg['expt_name']}.csv")
-    # pd.concat([val_agg_df, test_agg_df], axis=1).plot.bar()
-    # import matplotlib.pyplot as plt
-    # plt.show()
+            df += pd.read_csv(Path(__file__).parent.parent / 'output' / 'metrics' / f"{expt_id}_{phase}.csv",
+                              index_col=0) / total
+        df.to_csv(Path(__file__).parent.parent / 'output' / 'metrics' / f"{cfg['expt_name']}_{phase}.csv")
 
 
 if __name__ == '__main__':
